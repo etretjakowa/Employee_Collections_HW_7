23f7a99b6ebbdad8a855f159cc96ba8da90ce15a
etretjakova <etretjakowa@yandex.ru>
2024-10-23 22:07:40 +0300

    initial commit

---
+++ src/main/java/pro/sky/Employee/Collections/HW7/EmployeeCollectionsHw7Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Employee.Collections.HW7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCollectionsHw7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCollectionsHw7Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Employee/Collections/HW7/EmployeeCollectionsHw7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Employee.Collections.HW7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCollectionsHw7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
